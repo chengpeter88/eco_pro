635f6eda42306bc0af060076ee57ec86a1a6988a
chengpeter88 <108580387+chengpeter88@users.noreply.github.com>
2023-09-19 15:50:07 +0800

    Update class.py

--- class.py
+++ class.py
@@ -45,3 +45,37 @@ game={
     }
 
 game[("top","left")]
+
+###set 隨便亂排 一堆東西 沒有排序 沒法直接取值
+
+
+list_example = [1, 2, 3, 4, 5]
+dict_example = {"name": "John", "age": 36, "birthday": "01/01/1985"}
+
+# change values
+list_example[0] = 10
+dict_example["name"] = "Jane"
+print(list_example)
+print(dict_example)
+
+# delete values
+del list_example[4]
+del dict_example["birthday"]
+print(list_example)
+print(dict_example)
+
+# add values
+list_example.append(6) # add 6 to the end of the list
+list_example.insert(0, 7) # add 4 to the beginning of the list
+dict_example["birthday"] = "01/01/1985"
+print(list_example)
+print(dict_example)
+
+###sort 排序 list 中東西排序
+####append 裡面只能放入int
+###不用再回存
+########################
+###numpy array 特性
+X=np.ones(3)
+y=np.array([2,4,6])
+#p.array((2,4,6))

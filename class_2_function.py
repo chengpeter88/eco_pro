472f0c0ab9e48b8e78772e9e7810b4b915d9cb55
chengpeter88 <108580387+chengpeter88@users.noreply.github.com>
2023-09-26 15:07:15 +0800

    Update class_2_function.py

--- class_2_function.py
+++ class_2_function.py
@@ -24,7 +24,7 @@ result=f(1)  ### send back 3 to result
 print(result)
 ##### return mean send back value
 
-
+### no change under the hood
 def changeFirstToZero_keepx(x):
     x_copy=x.copy()
     x_copy[0]=0
@@ -43,3 +43,52 @@ xx
 yy=[-3,1,4]
 changeFirstToZero_keepx(yy)
 yy
+
+import numpy as np
+def f(x):
+    if x<=0:
+        return False
+    return np.log(x)
+f(-10)
+#善用return可以使函數更有效率的提早結束
+
+
+def f(x):
+    def power3(z):
+        return z**3 
+    return power3(x)
+
+
+x=4
+def a():
+    y=1
+    def b(z): 
+        print(y)
+        print(x)
+        print(z)
+    b(3)
+
+a()
+
+########anonoymous function
+f= lambda X:x**2
+
+#######
+def preference(x,y):
+    return x**0.8*y**0.2
+
+consumer = {
+    'name': 'John Doe',
+    'age': 30,
+    'preference': preference
+}
+
+consumer = {
+    'name': 'John Doe',
+    'age': 30,
+    'preference': lambda x,y: x**0.8*y**0.2
+}
+
+def cd(x,y,alpha):
+    return x**alpha*y**(1-alpha)
+cd(3,4,0.5)

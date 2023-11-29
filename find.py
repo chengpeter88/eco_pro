8578345a60cd7307cd14202f7aac7355fc00e59f
chengpeter88 <108580387+chengpeter88@users.noreply.github.com>
2023-11-29 13:55:24 +0800

    11/29

--- find.py
+++
@@ -1,6 +0,0 @@
-def find_max(num_list):
-    max = 0 
-    for number in num_list:
-        if number > max:
-            max = number
-    print(max)
--- prac.py
+++
@@ -1,6 +0,0 @@
-import os 
-os.getcwd()
-
-from find import find_max
-num= [2, 3, 4,55]  
-find_max(num)  

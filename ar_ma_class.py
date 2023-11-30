2c4d35e5089a70d772454c864cd3399032a8a462
chengpeter88 <108580387+chengpeter88@users.noreply.github.com>
2023-11-30 15:45:27 +0800

    11/30

---
+++ ar_ma_class.py
@@ -0,0 +1,150 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+#################### ARMA class ####################
+class AR:
+    def __init__(self, *args, epsilon, Y0):
+        self.phi = np.array(args)
+        self.epsilon = epsilon
+        self.Y0 = np.array(Y0)
+        self.memory = np.array([])
+        self.YPast = np.array(self.Y0)
+    def simulate_nPeriods(self, n=1):
+        simulate_nPeriods(self, n)
+    
+    #def clear_memory(self):
+    #    self.memory = np.array([])
+    
+    def statistics(self):
+        return statistics(self)
+    
+    def plot(self):
+        plot(self)
+    
+    def save(self, file_name):
+        save(self, file_name)
+#################### MA class ####################
+class MA:
+    def __init__(self, q, theta, mu, sigma):
+        if not isinstance(q, int) :
+            raise ValueError(" q must be integers")
+        if len(theta) != q:
+            raise ValueError("Length of theta must be equal to q")
+    
+        self.q = q  # MA model order
+        self.theta = np.array(theta)  # MA coefficients
+        self.mu = mu  # Mean of the series
+        self.sigma = sigma  # Standard deviation of the noise
+        self.memory = np.zeros(q)  # Initialize memory to store past noise values
+
+        # """條件需要符合 q 是整數，否則會報錯，需要有套件"""
+    
+    def simulate(self, n=1):
+        """
+        預設參數為模擬一期，可以自行設定調用模擬期數
+        """
+        return simulate(self, n)
+    
+    def statistics(self):
+        return statistics(self)
+    
+    def covariance(self, k):
+        if isinstance(k, int):
+            return covariance(self, k)
+        else:
+            raise ValueError("k must be an integer")
+
+    def plot(self):
+        plot(self, self.q) 
+    
+    def save(self, file_name):
+        save(self, file_name)  
+
+
+
+
+#################### helpers  fuctions ####################
+# AR helpers    
+
+def simulate_onePeriod(ar, eps):
+    y_onePeriod_ahead = ar.phi@ar.YPast + eps
+    ar.YPast = np.append(y_onePeriod_ahead, ar.YPast[:-1])
+    ar.memory = np.append(ar.memory, y_onePeriod_ahead)
+
+def simulate_nPeriods(ar, n):
+    eps = ar.epsilon(n)
+    for i in range(n):
+        simulate_onePeriod(ar, eps[i])
+
+def statistics(ar):
+    mu_val = np.mean(ar.memory)
+    sigma_val = np.std(ar.memory)
+    corr_val = np.corrcoef(ar.memory[1:], ar.memory[:-1])[0,1]  ## 2X2 矩陣[0,1]位子是相關係數  
+    return f"平均數:{mu_val:.4f}, 變異數:{sigma_val:.4f}, 相關係數:{corr_val:.4f}"
+
+def plot(ar):
+    plt.style.use('ggplot')
+    plt.figure(figsize=(10, 4))
+    plt.plot(ar.memory,marker='o')
+    plt.title("AR(1) process")
+    plt.xlabel("preiod")
+    plt.ylabel("y")
+    plt.show()
+
+def save(ar, file_name):
+    df = pd.DataFrame(ar.memory)
+    if file_name.endswith('.xlsx'):
+        df.to_excel(file_name, index=False)
+    elif file_name.endswith('.txt'):
+        df.to_csv(file_name, index=False, sep='\t')
+    elif file_name.endswith('.csv'):
+        df.to_csv(file_name, index=False)
+    else:
+        raise ValueError("file_name must end with .xlsx or .txt or .csv")
+
+# MA helpers    
+
+def simulate(self, n=1):
+    noise = np.random.normal(0, self.sigma, n + self.q)  # Generate noise   
+    X = np.zeros(n)
+    for t in range(n):
+        X[t] = self.mu + noise[t + self.q]  # Current noise term
+        for i in range(min(self.q, t+1)):
+             X[t] += self.theta[i] * noise[t + self.q - i - 1]  # Past noise terms
+        self.memory = np.append(self.memory, X[t])  # Update memory with new value
+    return X
+
+def plot(ma, q):
+    plt.style.use('ggplot')
+    plt.figure(figsize=(10, 4))
+    plt.plot(ma.memory, marker='o')
+    plt.title(f"MA({q}) Process")  
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.show()
+
+def statistics(ma):
+    mean = ma.mu
+    variance = ma.sigma**2 * (1 + np.sum(ma.theta**2))
+    return f"平均數:{mean:.4f}, 變異數:{variance:.4f}"
+
+def covariance(ma, k):
+    for k in range(1,k+1):
+        if k > ma.q:
+            covariance = 0
+        else:
+            theta_padded = np.append(ma.theta, np.zeros(k))  # Pad theta with zeros for lag
+            covariance = ma.sigma**2 * np.sum(theta_padded[k:ma.q+k] * ma.theta[:ma.q])
+        print(f"共變異數（lag {k}）: {covariance:.4f}")
+
+def save(ma, file_name):
+    df = pd.DataFrame(ma.memory)
+    if file_name.endswith('.xlsx'):
+        df.to_excel(file_name, index=False)
+    elif file_name.endswith('.txt'):
+        df.to_csv(file_name, index=False, sep='\t')
+    elif file_name.endswith('.csv'):
+        df.to_csv(file_name, index=False)
+    else:
+        raise ValueError("file_name must end with .xlsx or .txt or .csv")
+
---
+++ arma_final.py
@@ -0,0 +1,76 @@
+#################### ARMA class ####################
+class ARMA:
+    ### Q:if p,q 的長度不等於 phi,theta 的長度，會報錯
+    def __init__(self, p, q, phi, theta, c, mu,sigma):
+        if not isinstance(p, int) or not isinstance(q, int):
+            raise ValueError("p and q must be integers")
+        if len(phi) != p:
+            raise ValueError("Length of phi must be equal to p")
+        if len(theta) != q:
+            raise ValueError("Length of theta must be equal to q")
+        self.p = p  # Order of the AR component
+        self.q = q  # Order of the MA component
+        self.phi = np.array(phi)  # AR coefficients
+        self.theta = np.array(theta)  # MA coefficients
+        self.c = c  # Constant term
+        self.sigma = sigma  # Standard deviation of the noise
+        self.mu = mu  #mean  of the noise
+        self.memory = np.zeros(max(p, q))  # Initialize memory for AR and MA parts
+        self.noise_memory = np.zeros(q)  # Memory for noise terms in MA part
+   
+    def simulate(self, n=1):
+        simulate(self, n) 
+
+    def statistics(self):
+        return statistics(self)
+
+    def plot(self):
+        plot(self, self.p, self.q)
+    
+    def save(self, file_name):
+        save(self, file_name)
+
+
+
+
+
+#################### ARMA hepler function ####################
+
+def simulate(arma, n=1):
+    X = np.zeros(n)
+    for t in range(n):
+        noise = np.random.normal(arma.mu, arma.sigma)
+            # AR part
+        X[t] = arma.c + noise
+        for i in range(min(t, arma.p)):
+            X[t] += arma.phi[i] * arma.memory[i]
+            # MA part
+        for j in range(min(t+1, arma.q)):
+            X[t] += arma.theta[j] * arma.noise_memory[j]
+        arma.memory = np.concatenate(([X[t]], arma.memory))
+        arma.noise_memory = np.concatenate(([noise], arma.noise_memory))
+    return X
+
+def statistics(arma):
+    mean = arma.c / (1 - np.sum(arma.phi))
+    variance = arma.sigma**2 * (1 + np.sum(arma.theta**2) + 2 * np.sum(arma.phi**2))
+    return f"平均數:{mean:.4f}, 變異數:{variance:.4f}"
+def plot(arma, p, q):
+    plt.style.use('ggplot')
+    plt.figure(figsize=(10, 4))
+    plt.plot(arma.memory, marker='o')
+    plt.title(f"ARMA({p}, {q}) Process")
+    plt.xlabel("period")
+    plt.ylabel("Value")
+    plt.show()
+
+def save(arma, file_name):
+    df = pd.DataFrame(arma.memory)
+    if file_name.endswith('.xlsx'):
+        df.to_excel(file_name, index=False)
+    elif file_name.endswith('.txt'):
+        df.to_csv(file_name, index=False, sep='\t')
+    elif file_name.endswith('.csv'):
+        df.to_csv(file_name, index=False)
+    else:
+        raise ValueError("file_name must end with .xlsx or .txt or .csv")
